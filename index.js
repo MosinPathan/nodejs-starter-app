d55cc0f05a4128f31500bc44f1788593e9728ce0
MosinPathan <165696137+MosinPathan@users.noreply.github.com>
2024-10-24 18:42:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 
 // To server static content like styles
 app.use(express.static('public'));
-
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) =>
     // renders text only
     //res.send('Hello World!'));
