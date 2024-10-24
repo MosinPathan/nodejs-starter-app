babb964e00f54a7f4d480cd48ad7663e4ff25dd1
MosinPathan <165696137+MosinPathan@users.noreply.github.com>
2024-10-24 18:49:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ app.get('/', (req, res) =>
     res.render('index')
 )
 // server will listen on http://localhost:3000/
-app.listen(3000, () => console.log('app running'))
+app.listen(process.env.PORT || 3000, () => console.log('app running'))
